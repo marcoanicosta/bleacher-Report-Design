aea5364d0f10714380a6fa6caac97dcea211089d
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2019-11-03 19:44:41 +0000

    BEM refactor main + nav

---
+++ website/assets/js/header.js
@@ -0,0 +1,8 @@
+var button = document.querySelector(".header__nav-toggle");
+var container =
+on ==
+button.addEventListener('click', function(){
+  if(on ==  false){
+ container.classList.add("is-open");
+}
+});
